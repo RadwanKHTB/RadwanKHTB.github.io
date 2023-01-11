35408c13a4cf6e8c6cf4f86fe010ad68ee06555e
RadwanKHTB <74743807+RadwanKHTB@users.noreply.github.com>
2023-01-12 00:54:46 +0400

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import * as THREE from 'https://unpkg.com/three@0.148.0/build/three.module.js';
+import * as THREE from 'three';
 import { OrbitControls } from 'https://unpkg.com/three@0.148.0/examples/jsm/controls/OrbitControls.js';
 
 
